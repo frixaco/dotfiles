8717988181e51d36dc7ff9227154c28db3c86d77
Rustam <rustam21ashurmatov@gmail.com>
2022-08-02 14:31:53 +0500

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,36 @@
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+source ~/.config/fish/alias.fish
 
-function fish_greeting
-	echo "Always read errors."
+# Load all saved ssh keys
+/usr/bin/ssh-add -A ^/dev/null
+
+# for WSL2 (Ubuntu) (not Distrod)
+# echo "qwe" | sudo -S bash ~/startup.sh
+# clear
+
+# Remove greeting
+set -U fish_greeting
+
+# Enable Ctrl+Backspace/Delete word deletion
+function fish_user_key_bindings
+    # ctrl + backspace to delete previous word
+    bind \cH backward-kill-path-component
+    # ctrl + delete to to delete forward word
+    bind \[3\;5~ kill-word
 end
 
-source /opt/asdf-vm/asdf.fish
+# Vim
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
 
-set -gx EDITOR nvim
+# Starship prompt
+# starship init fish | source
 
-starship init fish | source
+# Rust
+set -Ua fish_user_paths $HOME/.cargo/bin
+
+# Bun
+set -Ux BUN_INSTALL "/home/frixaco/.bun"
+set -px --path PATH "/home/frixaco/.bun/bin"
+
+source ~/.asdf/asdf.fish
