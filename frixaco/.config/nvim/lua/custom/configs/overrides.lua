b3acdc0b3ad71b52825fda9069c95162da6aa7fe
frixaco <rustam21ashurmatov@gmail.com>
2023-10-25 15:50:52 +0500

    Python support

--- frixaco/.config/nvim/lua/custom/configs/overrides.lua
+++ frixaco/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,79 +1,79 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
-    "graphql",
-    "go",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
+		"graphql",
+		"go",
+		"python",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "prettierd",
-    "eslint-lsp",
-    "tailwindcss-language-server",
-    "emmet-language-server",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"prettierd",
+		"eslint-lsp",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- python
-    "blackd-client",
-    "ruff-lsp",
+		-- python
+		"blackd-client",
+		"ruff-lsp",
+		"pyright",
 
-    -- golang
-    "gopls",
-  },
+		-- golang
+		"gopls",
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-    ignore = false,
-  },
+	git = {
+		enable = true,
+		ignore = false,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 M.telescope = {
-  pickers = {
-    find_files = {
-      find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
-    },
-  },
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+		},
+	},
 }
 
 return M
