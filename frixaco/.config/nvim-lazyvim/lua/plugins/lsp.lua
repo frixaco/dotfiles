71f51c0495feb05ff22450574372e087452f5ac0
frixaco <rustam21ashurmatov@gmail.com>
2023-11-05 13:29:17 +0500

    Setup LazyVim

--- frixaco/.config/nvim-lazyvim/lua/plugins/lsp.lua
+++ frixaco/.config/nvim-lazyvim/lua/plugins/lsp.lua
@@ -3,17 +3,76 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
+      diagnostics = {
+        virtual_text = false,
+        virtual_lines = true,
+        signs = true,
+        underline = false,
+      },
       ---@type lspconfig.options
       servers = {
         cssls = {},
         html = {},
         tsserver = {},
-        tailwindcss = {},
         eslint = {},
         emmet_language_server = {},
-        prettierd = {},
         pyright = {},
         ruff_lsp = {},
+        gopls = {
+          cmd = { "gopls" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+          root_dir = require("lspconfig.server_configurations.gopls").default_config.root_dir,
+          settings = {
+            gofumpt = true,
+            gopls = {
+              completeUnimported = true,
+              usePlaceholders = true,
+              analyses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+            },
+          },
+        },
+      },
+      setup = {
+        tailwindcss_language_server = function(_, opts)
+          require("tailwindcss").setup({
+            server = opts,
+            settings = {
+              tailwindCSS = {
+                experimental = {
+                  classRegex = {
+                    { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                    { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+                  },
+                },
+              },
+            },
+          })
+          return true
+        end,
+
+        gopls = function(_, opts)
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "gopls" then
+              if not client.server_capabilities.semanticTokensProvider then
+                local semantic = client.config.capabilities.textDocument.semanticTokens
+                client.server_capabilities.semanticTokensProvider = {
+                  full = true,
+                  legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                  },
+                  range = true,
+                }
+              end
+            end
+          end)
+          -- end workaround
+        end,
       },
     },
   },
@@ -30,7 +89,6 @@ return {
         "css-lsp",
         "html-lsp",
         "typescript-language-server",
-        "prettierd",
         "eslint-lsp",
 
         -- c/cpp stuff
