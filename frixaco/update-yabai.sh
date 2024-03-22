db908e6d706372a7cf8cedafcd914c557cf5d833
frixaco <rustam21ashurmatov@gmail.com>
2024-03-23 02:22:38 +0500

    (yabai) fix update script

--- frixaco/update-yabai.sh
+++ frixaco/update-yabai.sh
@@ -2,4 +2,4 @@
 
 export YABAI_CERT=
 codesign -fs "${YABAI_CERT:-yabai-cert}" "$(brew --prefix yabai)/bin/yabai"
-echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" >> /private/etc/sudoers.d/yabai
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" > /private/etc/sudoers.d/yabai
