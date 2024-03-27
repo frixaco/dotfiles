8618f08644549e4631f926ad4471d084a7c816bf
frixaco <rustam21ashurmatov@gmail.com>
2024-03-27 11:15:21 +0500

    (wezterm) add shortcuts to manage workspaces

--- frixaco/.config/wezterm/wezterm.lua
+++ frixaco/.config/wezterm/wezterm.lua
@@ -27,8 +27,8 @@ if is_windows() then
 end
 
 config.window_padding = {
-	left = 16,
-	right = 16,
+	left = 10,
+	right = 10,
 	top = 8,
 	bottom = 8,
 }
@@ -38,6 +38,51 @@ config.color_scheme = "Catppuccin Mocha"
 config.window_decorations = "INTEGRATED_BUTTONS"
 config.window_background_opacity = 0.98
 config.macos_window_background_blur = 20
+config.show_new_tab_button_in_tab_bar = false
+
+config.window_frame = {
+	font = wezterm.font({ family = "FiraCode Nerd Font", weight = "Bold" }),
+	font_size = 12.0,
+	active_titlebar_bg = "#1e1e2e",
+	inactive_titlebar_bg = "#1e1e2e",
+}
+config.inactive_pane_hsb = {
+	saturation = 0.8,
+	brightness = 0.6,
+}
+
+wezterm.on("update-right-status", function(window, pane)
+	window:set_left_status("")
+	window:set_right_status(" " .. window:active_workspace() .. " ")
+end)
+
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	if title and #title > 0 then
+		return title
+	end
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#1e1e2e"
+	local foreground = "#808080"
+
+	if tab.is_active then
+		background = "#cba6f7"
+		foreground = "#11111b"
+	elseif hover then
+		background = "#3b3052"
+		foreground = "#909090"
+	end
+
+	local title = tab_title(tab)
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = " " .. title .. " " },
+	}
+end)
 
 local function is_inside_vim(pane)
 	local tty = pane:get_tty_name()
@@ -81,6 +126,59 @@ config.keys = {
 	bind_if(is_outside_vim, "l", "CTRL", act.ActivatePaneDirection("Right")),
 	bind_if(is_outside_vim, "k", "CTRL", act.ActivatePaneDirection("Up")),
 	bind_if(is_outside_vim, "j", "CTRL", act.ActivatePaneDirection("Down")),
+
+	{
+		key = "R",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Rename workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line and line ~= "" then
+					-- Retrieve the current workspace name
+					local current_workspace = window:active_workspace()
+					-- Rename the workspace to the new name provided by the user
+					wezterm.mux.rename_workspace(current_workspace, line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "N",
+		mods = "CTRL|SHIFT",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						act.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+
+	{
+		key = "0",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+	},
 }
 
 for i = 1, 8 do
