e22532d171e3f2a75d8728d20504f6887f46e5fd
frixaco <rustam21ashurmatov@gmail.com>
2024-04-16 02:34:25 +0500

    (git) slight `cl` improvement

--- frixaco/github-clone.fish
+++ frixaco/github-clone.fish
@@ -1,3 +1,7 @@
 #!/usr/bin/env fish
 
-git clone git@github.com:$argv[1].git
+if test (count $argv) -eq 1
+    git clone git@github.com:$argv[1].git
+else if test (count $argv) -eq 2
+    git clone git@github.com:$argv[1].git $argv[2]
+end
