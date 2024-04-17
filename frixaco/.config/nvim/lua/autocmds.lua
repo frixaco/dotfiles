ca793d01eb2f529e7850e08bf6d2e57e00dc9b03
frixaco <rustam21ashurmatov@gmail.com>
2024-04-17 23:05:46 +0500

    (neovim) use Windows clipboard when yanking in WSL

--- frixaco/.config/nvim/lua/autocmds.lua
+++ frixaco/.config/nvim/lua/autocmds.lua
@@ -8,3 +8,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- WSL yank support
+local clip = '/mnt/c/Windows/System32/clip.exe' -- change this path according to your mount point
+
+if vim.fn.executable(clip) == 1 then
+  vim.api.nvim_create_augroup('WSLYank', { clear = true })
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = 'WSLYank',
+    callback = function()
+      if vim.v.event.operator == 'y' then
+        vim.fn.system(clip, vim.fn.getreg('0'))
+      end
+    end,
+  })
+end
