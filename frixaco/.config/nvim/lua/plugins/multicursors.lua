8cad180074a40d0b3c521bcb9a9ebea420175a7b
frixaco <rustam21ashurmatov@gmail.com>
2024-04-24 22:36:58 +0500

    (neovim) reminder to learn multi cursors plugin

--- frixaco/.config/nvim/lua/plugins/multicursors.lua
+++ frixaco/.config/nvim/lua/plugins/multicursors.lua
@@ -4,3 +4,13 @@ return {
     event = 'VeryLazy',
   },
 }
+
+--  TODO: wrap with "" each key in socialPosts
+-- "dealBreakers": ["No profanity"],
+-- "socialPosts": {
+--     instagram: 10,
+--     tiktok: None,
+--     youtube: None,
+--     twitter: None,
+--     facebook: None,
+-- },
