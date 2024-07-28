9fad6b73396a1941c4601de185b9df8e4c717be4
frixaco <rustam21ashurmatov@gmail.com>
2024-07-28 22:34:37 +0500

    (neovim) fix nvim-spider navigation

--- frixaco/.config/nvim/lua/plugins/nvim-spider.lua
+++ frixaco/.config/nvim/lua/plugins/nvim-spider.lua
@@ -2,7 +2,12 @@ return {
   {
     'chrisgrieser/nvim-spider',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      skipInsignificantPunctuation = false,
+      consistentOperatorPending = true,
+      subwordMovement = true,
+      customPatterns = {},
+    },
     config = function()
       vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Spider-w' })
       vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Spider-e' })
