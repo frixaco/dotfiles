7d388395eebbf2e4b1bdc44c1a4c5988d2260eb0
frixaco <rustam21ashurmatov@gmail.com>
2024-09-04 00:19:12 +0500

    remove yabai & unnecessary scripts

--- frixaco/github-clone.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-if test (count $argv) -eq 1
-    git clone git@github.com:$argv[1].git
-else if test (count $argv) -eq 2
-    git clone git@github.com:$argv[1].git $argv[2]
-end
--- frixaco/startWezterm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(LABEL=$(uuidgen) && yabai -m signal --add event="window_created" label="$LABEL" app="^WezTerm$" action="yabai -m window \$YABAI_WINDOW_ID --focus && yabai -m signal --remove $LABEL") && (/Applications/WezTerm.app/Contents/MacOS/wezterm connect local || open -na /Applications/WezTerm.app)
