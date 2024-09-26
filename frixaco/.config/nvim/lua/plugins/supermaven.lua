498736342927073f14edbf3f05c760887c9741f1
frixaco <rustam21ashurmatov@gmail.com>
2024-09-26 11:30:10 +0500

    (neovim) i said no to AI autocomplete

--- frixaco/.config/nvim/lua/plugins/supermaven.lua
+++ frixaco/.config/nvim/lua/plugins/supermaven.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    'supermaven-inc/supermaven-nvim',
-    lazy = false,
-    config = function()
-      require('supermaven-nvim').setup({
-        keymaps = {
-          accept_suggestion = '<M-a>',
-          clear_suggestion = '<C-]>',
-          accept_word = '<C-j>',
-        },
-        ignore_filetypes = { cpp = true },
-        log_level = 'off',
-        color = {
-          suggestion_color = '#ffffff',
-          cterm = 244,
-        },
-        disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false, -- disables built in keymaps for more manual control
-      })
-    end,
-  },
+  -- {
+  --   'supermaven-inc/supermaven-nvim',
+  --   lazy = false,
+  --   config = function()
+  --     require('supermaven-nvim').setup({
+  --       keymaps = {
+  --         accept_suggestion = '<M-a>',
+  --         clear_suggestion = '<C-]>',
+  --         accept_word = '<C-j>',
+  --       },
+  --       ignore_filetypes = { cpp = true },
+  --       log_level = 'off',
+  --       color = {
+  --         suggestion_color = '#ffffff',
+  --         cterm = 244,
+  --       },
+  --       disable_inline_completion = false, -- disables inline completion for use with cmp
+  --       disable_keymaps = false, -- disables built in keymaps for more manual control
+  --     })
+  --   end,
+  -- },
 }
