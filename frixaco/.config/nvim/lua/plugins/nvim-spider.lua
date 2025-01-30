026734fd0874a284965f97305e0837986e6554b6
frixaco <rustam21ashurmatov@gmail.com>
2025-01-30 13:53:37 +0500

    (neovim) spider fix(?)

--- frixaco/.config/nvim/lua/plugins/nvim-spider.lua
+++ frixaco/.config/nvim/lua/plugins/nvim-spider.lua
@@ -2,13 +2,13 @@ return {
   {
     'chrisgrieser/nvim-spider',
     event = 'VeryLazy',
-    opts = {
-      skipInsignificantPunctuation = false,
-      consistentOperatorPending = true,
-      subwordMovement = true,
-      customPatterns = {},
-    },
     config = function()
+      require('spider').setup({
+        skipInsignificantPunctuation = false,
+        consistentOperatorPending = true,
+        subwordMovement = true,
+        customPatterns = {},
+      })
       vim.keymap.set({ 'n', 'o', 'x' }, 'w', "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Spider-w' })
       vim.keymap.set({ 'n', 'o', 'x' }, 'e', "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Spider-e' })
       vim.keymap.set({ 'n', 'o', 'x' }, 'b', "<cmd>lua require('spider').motion('b')<CR>", { desc = 'Spider-b' })
