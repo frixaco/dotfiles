3f797caaa6fa352318b9664940675efd4869ed19
frixaco <rustam21ashurmatov@gmail.com>
2025-03-27 01:58:53 +0500

    (neovim) update blink config

--- frixaco/.config/nvim/lua/plugins/blink.lua
+++ frixaco/.config/nvim/lua/plugins/blink.lua
@@ -15,28 +15,69 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'default' },
+      -- Enables keymaps, completions and signature help when true (doesn't apply to cmdline or term)
+      --
+      -- If the function returns 'force', the default conditions for disabling the plugin will be ignored
+      -- Default conditions: (vim.bo.buftype ~= 'prompt' and vim.b.completion ~= false)
+      -- Note that the default conditions are ignored when `vim.b.completion` is explicitly set to `true`
+      --
+      -- Exceptions: vim.bo.filetype == 'dap-repl'
+      enabled = function()
+        return not vim.tbl_contains({ 'lua', 'markdown' }, vim.bo.filetype)
+      end,
 
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
+      -- Disable cmdline
+      cmdline = { enabled = true },
+
+      completion = {
+        -- 'prefix' will fuzzy match on the text before the cursor
+        -- 'full' will fuzzy match on the text before _and_ after the cursor
+        -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
+        keyword = { range = 'full' },
+
+        -- Disable auto brackets
+        -- NOTE: some LSPs may add auto brackets themselves anyway
+        accept = { auto_brackets = { enabled = false } },
+
+        -- Don't select by default, auto insert on selection
+        list = { selection = { preselect = false, auto_insert = true } },
+        -- or set via a function
+        -- list = { selection = {
+        --   preselect = function(ctx)
+        --     return vim.bo.filetype ~= 'markdown'
+        --   end,
+        -- } },
+
+        menu = {
+          -- Don't automatically show the completion menu
+          auto_show = true,
+
+          -- nvim-cmp style menu
+          draw = {
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind' },
+            },
+          },
+        },
+
+        -- Show documentation when selecting a completion item
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+
+        -- Display a preview of the selected item on the current line
+        ghost_text = { enabled = true },
       },
 
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp' }, -- , 'snippets', 'buffer', 'path'
+        -- Remove 'buffer' if you don't want text completions, by default it's only enabled when LSP returns no items
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
+
+      -- Use a preset for snippets, check the snippets documentation for more information
+      -- snippets = { preset = 'default' | 'luasnip' | 'mini_snippets' },
+
+      -- Experimental signature help support
+      signature = { enabled = true },
     },
-    opts_extend = { 'sources.default' },
   },
 }
