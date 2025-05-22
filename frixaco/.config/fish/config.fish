e7ba47098bf29603dac55aa67dabea371ed60cd5
frixaco <rustam21ashurmatov@gmail.com>
2025-05-23 01:08:02 +0500

    (alacritty+fish+lazygit) sync

--- frixaco/.config/fish/config.fish
+++ frixaco/.config/fish/config.fish
@@ -34,6 +34,7 @@ set -gx BUN_INSTALL ~/.bun
 set -gx DENO_INSTALL $HOME/.deno
 
 fish_add_path /opt/homebrew/opt/grep/libexec/gnubin 
+fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.orbstack/bin $HOME/stuff/flutter/bin $DENO_INSTALL/bin $BUN_INSTALL/bin ~/.amplify/bin ~/.local/bin $ANDROID_HOME/emulator $ANDROID_HOME/platform-tools $EMSDK/upstream/bin ~/.cargo/bin
 
 alias l="eza --color=always --icons --all --long --time modified --sort modified --no-permissions --octal-permissions --git --smart-group"
