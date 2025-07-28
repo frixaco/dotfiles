dbb116c142e7e4401740ba1d7b3a5e2a0d1327ba
frixaco <rustam21ashurmatov@gmail.com>
2025-07-29 01:16:05 +0500

    (neovim) fix clipboard in wsl; use Fish shell in :term

--- frixaco/.config/nvim/init.lua
+++ frixaco/.config/nvim/init.lua
@@ -14,6 +14,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- WSL yank support
+local clip = '/mnt/c/Windows/System32/clip.exe' -- change this path according to your mount point
+
+if vim.fn.executable(clip) == 1 then
+  vim.api.nvim_create_augroup('WSLYank', { clear = true })
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = 'WSLYank',
+    callback = function()
+      if vim.v.event.operator == 'y' then
+        vim.fn.system(clip, vim.fn.getreg('0'))
+      end
+    end,
+  })
+end
+
+vim.o.shell = 'fish'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.o.number = true
