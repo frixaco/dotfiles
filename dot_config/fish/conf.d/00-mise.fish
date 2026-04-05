d8d3fc61e482a0769bb4036352d4ec2b7c0084f6
frixaco <rustam21ashurmatov@gmail.com>
2026-04-05 12:04:19 +0500

    (fish) support linuxbrew for non-interactive fish shells

--- dot_config/fish/conf.d/00-mise.fish
+++
@@ -1,7 +0,0 @@
-fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/mise/bin
-
-if test -x /opt/homebrew/opt/mise/bin/mise
-    /opt/homebrew/opt/mise/bin/mise activate fish | source
-else if test -x /opt/homebrew/bin/mise
-    /opt/homebrew/bin/mise activate fish | source
-end
