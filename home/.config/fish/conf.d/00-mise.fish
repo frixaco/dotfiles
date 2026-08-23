2877907cf575797bc088b45699f2fe5e61e7ff87
frixaco <rustam21ashurmatov@gmail.com>
2026-08-23 22:43:35 +0500

    (mise) switch to bash installer

--- home/.config/fish/conf.d/00-mise.fish
+++ home/.config/fish/conf.d/00-mise.fish
@@ -1,17 +1,9 @@
 if test (uname) = "Darwin"
-    fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/mise/bin
-
-    if test -x /opt/homebrew/opt/mise/bin/mise
-        /opt/homebrew/opt/mise/bin/mise activate fish | source
-    else if test -x /opt/homebrew/bin/mise
-        /opt/homebrew/bin/mise activate fish | source
-    end
+    fish_add_path /opt/homebrew/bin /opt/homebrew/sbin
 else if test (uname) = "Linux"
-    fish_add_path /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin /home/linuxbrew/.linuxbrew/opt/mise/bin
+    fish_add_path /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin
+end
 
-    if test -x /home/linuxbrew/.linuxbrew/opt/mise/bin/mise
-        /home/linuxbrew/.linuxbrew/opt/mise/bin/mise activate fish | source
-    else if test -x /home/linuxbrew/.linuxbrew/bin/mise
-        /home/linuxbrew/.linuxbrew/bin/mise activate fish | source
-    end
+if test -x ~/.local/bin/mise
+    ~/.local/bin/mise activate fish | source
 end
